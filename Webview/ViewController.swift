00ae92a005a9ce4545c534bd0618a28d61382a12
Zaur Giyasov <zaurusnnov@icloud.com>
2018-03-14 20:11:08 +0300

    simple webview

--- Webview/ViewController.swift
+++ Webview/ViewController.swift
@@ -7,19 +7,26 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myRequest()
+    }
+    
+    func myRequest() {
+        let url = URL(string: "https://www.yandex.ru")
+        let request = URLRequest(url: url!)
+        webView.load(request)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
-
 
 }
 
